4d911cc666ac99690d52d0188f42ad507c26218c
huynhtoan3152004 <toanhhqe180078@fpt.edu.vn>
2025-09-17 17:03:52 +0700

    AccountService add

---
+++ aDealerEDVMS.Service.ToanHH/SystemUserAccountService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aDealerEDVMS.Service.ToanHH
+{
+    public interface ISystemUserAccountService
+    {
+        Task<SystemUserAccount> GetUserAccountAsync(string username, string password); 
+    }
+    public class SystemUserAccountService : ISystemUserAccountService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SystemUserAccountService()
+        {
+            _unitOfWork ??= new UnitOfWork();
+        }
+
+        public async Task<SystemUserAccount> GetUserAccountAsync(string userName, string password)
+        {
+            return await _unitOfWork.SystemUserAccountRepository.GetUserAccountAsync(userName, password);
+        }
+    }
+}
